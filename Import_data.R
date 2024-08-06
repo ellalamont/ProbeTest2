973ab979de606513a5f05ef422d6e8757620d51a
ellalamont <elamont@uw.edu>
2024-08-06 07:50:28 -0700

    Added import data

---
+++ Import_data.R
@@ -0,0 +1,54 @@
+# Probe Prep testing 2
+# July probes with test samples
+# 8/6/24
+
+
+################################################
+################ LOAD PACKAGES #################
+
+library(ggplot2)
+library(tidyverse)
+library(ggpubr)
+library(RColorBrewer)
+library(knitr)
+library(plotly)
+library(ggprism) # for add_pvalue()
+library(rstatix) # for adjust_pvalue
+library(ggpmisc) # https://stackoverflow.com/questions/7549694/add-regression-line-equation-and-r2-on-graph
+library(ggrepel)
+library(pheatmap)
+library(ggplotify) # To convert pheatmaps to ggplots
+library(corrplot)
+library(ggcorrplot)
+library(ggfortify) # To make pca plots with plotly
+
+
+
+cbPalette_1 <- c("#999999", "#E69F00") # Gold and Grey
+cbPalette_1.5 <- c("#E69F00", "#999999") # Gold and Grey
+cbPalette_2 <- c( "#0072B2", "#999999") # Blue and Grey
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+cbPalette2 <-  c("#bfbfbf", "#56B4E9")
+cbPalette3 <-  c("#bfbfbf", "#E69F00")
+cbPalette4 <- c("#56B4E9", "#009E73", "#F0E442")
+c25 <- c(
+  "dodgerblue2", "#E31A1C", "green4",
+  "#6A3D9A","#FF7F00","black", "gold1",
+  "skyblue2", "#FB9A99","palegreen2","#CAB2D6",
+  "#FDBF6F","gray70", "khaki2","maroon", "orchid1", "deeppink1", "blue1", "steelblue4","darkturquoise", "green1", "yellow4", "yellow3","darkorange4", "brown"
+)
+c12 <- c("dodgerblue2", "#E31A1C", "green4", "#6A3D9A", "#FF7F00", "black", "palegreen2", "gray70", "maroon", "orchid1", "darkturquoise", "darkorange4") 
+c16 <- c("dodgerblue2", "#E31A1C", "green4", "#6A3D9A", "#FF7F00", "black","gold1", "#FB9A99", "#CAB2D6", "palegreen2", "gray70", "maroon", "orchid1", "blue1", "darkturquoise", "darkorange4") 
+
+
+# Stop scientific notation
+options(scipen = 999) 
+# options(scipen = 0) # To revert back to default
+
+###########################################################
+############### IMPORT PIPELINE SUMMARY DATA ##############
+
+my_df <- read.csv("Pipeline.Summary.Details.csv")
+
+
+
